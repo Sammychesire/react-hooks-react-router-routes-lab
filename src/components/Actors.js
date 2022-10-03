d6b574337a150a9f3b0b3ffa05828d883a3506fd
bob <sammy.chesire@student.moringaschool.com>
2022-10-03 14:12:24 +0300

    completed assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,21 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>Actors Page</h1>
+    {actors.map((actor,index)=> {
+      return (
+        <React.Fragment key={index}>
+          <p>{actor.name}</p>
+          <ul>
+            {actor.movies.map((movie, i) => (
+              <li key={i}>{movie}</li>
+            ))}
+          </ul>
+          </React.Fragment>
+      )
+    })}
+  </div>;
 }
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,23 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+return(
+  <div>
+    <h1>Directors Page</h1>
+    {directors.map((director,index) => {
+      return (
+        <React.Fragment key={index}>
+         <p>{director.name}</p>
+         <ul>
+          {director.movies.map((movie,i) => (
+            <li key={i}>{movie}</li>
+          ))}
+         </ul>
+        </React.Fragment>
+      )
+    })}
+  </div>
+)
 }
 
 export default Directors;
